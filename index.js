b0736ad8ba1c28672f0d6088f09b2778c5a7e2da
linhlinhtran <filougamespun0suy@hotmail.com>
2024-05-06 18:41:04 +0700

    update aptos task

---
+++ index.js
@@ -0,0 +1,13 @@
+function subsets(nums) {
+  const result = [];
+  backtrack(0, []);
+  return result;
+  function backtrack(start, current) {
+    result.push([...current]);
+    for (let i = start; i < nums.length; i++) {
+      current.push(nums[i]);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
+}
